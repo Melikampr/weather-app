cdd9b3bfb723504c83518bbd51425232ebeabfc3
Melika Mohsenipour <melikamohsenipour@gmail.com>
2023-11-09 16:43:13 -0800

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'dark': '#3b5998',
+        'light': '#addbef'
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+  ],
 }
 export default config
